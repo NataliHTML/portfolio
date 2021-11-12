41090ca9b6ae056cdca1832406dd6fd907688ee1
NataliHTML <nataly07081992>
2021-11-12 21:50:25 +0300

    portfolio

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,27 @@
 $(function () {
+	$('.menu__btn').on('click', function () {
+		$('.menu__list').toggleClass('menu__list--active');
+	});
+	$('.menu__list').on('click', function () {
+		$('.menu__list').removeClass('menu__list--active');
+	});
 
+	$('[data-scroll]').on('click', function (event) {
+		event.preventDefault();
 
+		blockId = $(this).data('scroll');
+		blockOffset = $(blockId).offset().top;
+
+		$("html,body").animate({
+			scrollTop: blockOffset + 50
+		}, 700);
+	});
+
+	$('[data-modal]').on('click', function (event) {
+		event.preventDefault();
+		$('#modal-resume').addClass('modal-show');
+	});
+	$('#modal-resume').on('click', function () {
+		$('#modal-resume').removeClass('modal-show');
+	});
 });
--- gulpfile.js
+++ gulpfile.js
@@ -28,7 +28,6 @@ function nunjucks() {
 function styles() {
 	return src('app/scss/*.scss')
 		.pipe(scss({ outputStyle: 'compressed' }))
-		// .pipe(concat())
 		.pipe(rename({
 			suffix: '.min'
 		}))
@@ -43,11 +42,6 @@ function styles() {
 function scripts() {
 	return src([
 		'node_modules/jquery/dist/jquery.js',
-		'node_modules/slick-carousel/slick/slick.js',
-		'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
-		'node_modules/rateyo/src/jquery.rateyo.js',
-		'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
-		'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
 		'app/js/main.js'
 	])
 		.pipe(concat('main.min.js'))
@@ -75,6 +69,7 @@ function images() {
 function build() {
 	return src([
 		'app/**/*.html',
+		'app/css/*.min.css',
 		'app/css/style.min.css',
 		'app/js/main.min.js',
 	], { base: 'app' })
